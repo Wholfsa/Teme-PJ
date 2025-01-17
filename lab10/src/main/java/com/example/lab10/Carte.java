72e2196976a2334e22464514278295048f25ed83
Andreea-Violeta Savu <133206204+Wholfsa@users.noreply.github.com>
2025-01-17 14:37:58 +0200

    Add files via upload

---
+++ lab10/src/main/java/com/example/lab10/Carte.java
@@ -0,0 +1,52 @@
+package com.example.lab10;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "carti")
+public class Carte {
+    @Id
+    @JsonProperty("isbn")
+    private String isbn;
+    @JsonProperty("titlu")
+    private String titlu;
+    @JsonProperty("autor")
+    private String autor;
+
+    public Carte() {
+
+    }
+
+    public Carte(String isbn, String titlu, String autor) {
+        this.isbn = isbn;
+        this.titlu = titlu;
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitlu() {
+        return titlu;
+    }
+
+    public void setTitlu(String titlu) {
+        this.titlu = titlu;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
